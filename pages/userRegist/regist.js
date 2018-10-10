e90a6afaad0cd946c74cce7c6a4e2d2ead13ea36
wj <425198161@qq.com>
2018-10-10 17:01:21 +0800

    Initial Commit

---
+++ pages/userRegist/regist.js
@@ -0,0 +1,12 @@
+const app = getApp()
+
+Page({
+    data: {
+      imgUrls: [
+        '../resource/images/0093.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+    }
+
+})
